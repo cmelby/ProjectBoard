484136aa58c44c9e3c3fff8ac5b017645f1ea592
Christopher Melby <melby.christopher@gmail.com>
2020-01-19 11:43:43 -0800

    adding seeds to database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO tasks(task_name)
+VALUE ("Product moc up");
+
+INSERT INTO tasks(task_name)
+VALUE ("Product Deck");
+
+INSERT INTO tasks(task_name)
+VALUE ("Brand Asset Creation", true);
