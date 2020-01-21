5f440d45ca116da5df919515e24750dd0fba5388
Christopher Melby <melby.christopher@gmail.com>
2020-01-21 14:47:46 -0800

    updating connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,12 @@ var mysql = require("mysql");
 
 //Instantiate connection....
 var connection = mysql.createConnection({
-    host: "	l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "root",
-    username: "s3meo6mynyeb3aj4",
-    password: "esh0ml87i64tts75",
-    database: "qdnukqk2x17t27la"
+    username: "koke3zgfd51zpiyu",
+    password: "ejupc8wfnbhk1hsr",
+    database: "tlp3hnevda0gi8q6"
 });
 connection.connect(function(err) {
     if (err) {
